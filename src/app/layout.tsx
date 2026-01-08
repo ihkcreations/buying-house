2a5762bc811eb9ff77149fe813cb21f2c1311ca4
ihkcreations <laramin.astra@gmail.com>
2026-01-08 19:12:35 +0600

    added loading bar on top of the app when a link is clicked

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Shell } from "@/components/layout/shell"; // <--- Import the new shell
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader 
+            color="#2563eb"
+            showSpinner={false} 
+            height={3}
+        />
         {/* The Shell handles showing/hiding the sidebar based on the URL */}
         <Shell>
             {children}
